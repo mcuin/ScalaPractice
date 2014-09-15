a6158533a2ff27af9eb30aa6e2d439196241a50a
Mykal Cuin <mykcuin@gmail.com>
2014-09-15 09:49:29 -0600

    Hello world scala. Working with functions an dobjects.

---
+++ class.scala
@@ -0,0 +1,12 @@
+class Complex(real: Double, imaginary: Double) {
+	def re = real
+	def im = imaginary
+	override def toString() = "" + re + (if (im < 0) "" else "+") + im + "i"
+}
+
+object complexNumbers {
+	def main(args: Array[String]) {
+		val c = new Complex(1.2, 3.4)
+		println("imaginary part: " + c.im)
+	}
+}
